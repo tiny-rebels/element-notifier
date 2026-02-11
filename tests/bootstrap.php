518d2b24e340e81397dd590d10c62997441899d8
Stefan Korfitz <stefan@korfitz.com>
2026-02-11 22:24:55 +0100

    added GatewayAPIDeleteScheduledMessageTest.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+// Composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load cURL-mocks, so that they are available in our test-run
+require __DIR__ . '/mocks/curl_mocks.php';
